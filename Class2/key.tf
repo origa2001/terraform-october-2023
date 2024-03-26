cf8bf56382e1f8da83dbdbb8016af799292934cd
origa2001 <153569665+origa2001@users.noreply.github.com>
2024-03-25 21:02:30 -0500

    Update key.tf

--- Class2/key.tf
+++ Class2/key.tf
@@ -1,5 +1,5 @@
-resource "aws_key_pair" "deployer" {
-  key_name   = "hello"
-  public_key = file("~/.ssh/id_rsa.pub")
-  tags = local.common_tags
-}
+#resource "aws_key_pair" "deployer" {
+ # key_name   = "hello"
+  # public_key = file("~/.ssh/id_rsa.pub")
+  # tags = local.common_tags
+#}
